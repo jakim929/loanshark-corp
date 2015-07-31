7e09d0705ea9716476979bac1a604c026df1dc19
James Kim <jakim929@gmail.com>
2015-07-31 17:02:58 +0900

    Initial Commit

---
+++ LoanShark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoanShark
+//
+//  Created by James Kim on 7/31/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
